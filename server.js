e6f18d1cff4416000158aa88faaac78c542aa630
Ashish <ash.ishu025g@gmail.com>
2019-12-12 23:02:42 +0530

    get api done

--- server.js
+++ server.js
@@ -6,12 +6,15 @@ app.get('/helloWorld', function (req, res) //Creating an api
     res.send('Hello World'); 
 });
 
+//http://localhost:9091/hellostudents?name=Any&age=22
+//{ name: 'Any', age: '22' }
 app.get('/hellostudents', function (req, res) //Creating an api
 { 
-    res.send('Hello Smart'); 
+    console.log(req.query);
+    res.send('Hello Smart Students'); 
 });
 
-let server = app.listen(8081, function () 
+let server = app.listen(9091, function () 
     { 
         let host = server.address().address;
         let port = server.address().port 
