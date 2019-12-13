032b1c125f20cba18e74bfe2a3db40f8a7bb1da8
Ashish <ash.ishu025g@gmail.com>
2019-12-13 23:04:33 +0530

    writing with js and wild card routing

--- server.js
+++ server.js
@@ -1,17 +1,37 @@
 let express = require("express"),
-app = express(); // instantiates express
+fs = require("fs"),
+app = express(); // instantiates express application
 
+//REST
 app.get('/helloWorld', function (req, res) //Creating an api
 { 
     res.send('Hello World'); 
 });
 
-//http://localhost:9091/hellostudents?name=Any&age=22
-//{ name: 'Any', age: '22' }
 app.get('/hellostudents', function (req, res) //Creating an api
 { 
-    console.log(req.query);
-    res.send('Hello Smart Students'); 
+    console.log(req.query);    
+    //fs = create the write stream and save the data
+    var writerStream = fs.createWriteStream('output.json');
+    writerStream.write(JSON.stringify({
+        user:req.query,
+        status: 200
+    }) ,'UTF8'); 
+    // Mark the end of file 
+    writerStream.end(); 
+    res.json({
+        user:req.query,
+        status: 200
+    });
+    //res.send('Hello Smart Students'); 
+});
+
+app.post("/sendmehtml",(req,res)=>{
+    res.send("<h1>This is html from express route</h1>").status(400);
+});
+
+app.get("*",(req,res)=>{
+    res.sendFile("index.html",{ root: __dirname });
 });
 
 let server = app.listen(9091, function () 
