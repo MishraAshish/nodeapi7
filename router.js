c4115cc5a9ef017895904df7ec494bf4a60adf09
Ashish <ash.ishu025g@gmail.com>
2019-12-16 22:53:00 +0530

    router

---
+++ router.js
@@ -0,0 +1,63 @@
+let express = require('express'),
+router = express.Router({caseSensitive:true});
+
+
+//REST
+router.get('/helloWorld', function (req, res) //Creating an api
+{ 
+    res.send('Hello World'); 
+});
+
+router.get('/getstudnts',(req, res)=>{
+    var outputfile = require("./output.json");
+    console.log(outputfile);
+    res.send(outputfile);
+})
+
+router.get('/hellostudents', function (req, res) //Creating an api
+{ 
+    console.log(req.query);    
+    //fs = create the write stream and save the data
+    var path = "output.json";
+    fs.stat(path, function(err, stat) {
+        console.log("Test", err);
+        if(err == null) {
+            fs.appendFile('output.json'," ,"+JSON.stringify({
+            user:req.query,
+            status: 200
+        }),"UTF8",()=>{});
+      }else{
+        fs.appendFile('output.json',JSON.stringify({
+            user:req.query,
+            status: 200
+        }),"UTF8",()=>{});
+      }
+    });
+    
+    // Mark the end of file 
+    
+    res.json({
+        user:req.query,
+        status: 200
+    });
+    //res.send('Hello Smart Students'); 
+});
+
+router.post("/sendmehtml",(req,res)=>{
+    res.send("<h1>This is html from express route</h1>").status(400);
+});
+
+router.get("*",(req,res)=>{
+    res.sendFile("index.html",{ root: __dirname });
+});
+
+// let server = app.listen(9091, function () 
+//     { 
+//         let host = server.address().address;
+//         let port = server.address().port 
+//        console.log("Example app listening at http://:", host, port) 
+//     }
+// )
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,43 +1,16 @@
 let express = require("express"),
 fs = require("fs"),
-app = express(); // instantiates express application
+app = express(), // instantiates express application
+router = require("./router"),
+port = process.env.PORT || 9090,
+bodyParser = require("body-parser");
 
-//REST
-app.get('/helloWorld', function (req, res) //Creating an api
-{ 
-    res.send('Hello World'); 
-});
+//app.use(cors());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json({limit:'200mb', extended:false}));
 
-app.get('/hellostudents', function (req, res) //Creating an api
-{ 
-    console.log(req.query);    
-    //fs = create the write stream and save the data
-    var writerStream = fs.createWriteStream('output.json');
-    writerStream.write(JSON.stringify({
-        user:req.query,
-        status: 200
-    }) ,'UTF8'); 
-    // Mark the end of file 
-    writerStream.end(); 
-    res.json({
-        user:req.query,
-        status: 200
-    });
-    //res.send('Hello Smart Students'); 
-});
+app.use('/',router);
 
-app.post("/sendmehtml",(req,res)=>{
-    res.send("<h1>This is html from express route</h1>").status(400);
-});
+console.log("we are listening at", port);
+app.listen(port);
 
-app.get("*",(req,res)=>{
-    res.sendFile("index.html",{ root: __dirname });
-});
-
-let server = app.listen(9091, function () 
-    { 
-        let host = server.address().address;
-        let port = server.address().port 
-       console.log("Example app listening at http://:", host, port) 
-    }
-)
