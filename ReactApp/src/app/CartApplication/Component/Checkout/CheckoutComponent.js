c41d1f584abcbb410d28f9ba418485ae52fa151e
Ashish <ash.ishu025g@gmail.com>
2020-01-24 22:51:13 +0530

    usestate method

--- ReactApp/src/app/CartApplication/Component/Checkout/CheckoutComponent.js
+++ ReactApp/src/app/CartApplication/Component/Checkout/CheckoutComponent.js
@@ -1,23 +1,41 @@
-import React from "react";  
+import React, {useState} from "react";  
 import CheckoutList from "../../Container/Cart/CartListContainer";  
 import CheckoutSummary from "../../Container/Cart/CartSummayContainer";  
  
 export default function Checkout(props){  
- 
-console.log("props at checkout",props)  
-return(  
-        <div className="checkoutComponent">  
-            <h2>Checking out </h2>  
-            <div className="col-md-12">  
-            <b>Hi, {props.user.firstName != "" ? props.user.firstName: "Tom"}  
-            </b>  
-            {props.coupon>0 ? <p>{"Your Generated Coupon is - "+props.coupon}</p>
-            :<p>you don't have Generated coupon  
-            </p> }  
-            
-            </div>  
-            <CheckoutList donotshowInputBox={true}/>  
-            <CheckoutSummary/>  
-        </div>  
-)  
+     
+    const [processToCheckout, setProcess] = useState(false);
+
+    const checkoutProcess=()=>{
+        setProcess(true);  
+    }
+
+    console.log("props at checkout",props)  
+
+    return(
+        <React.Fragment>
+        {
+            !processToCheckout ?
+            <div className="checkoutComponent">  
+                <h2>Checking out </h2>  
+                <div className="col-md-12">  
+                <b>Hi, {props.user.firstName != "" ? props.user.firstName: "Tom"} </b>  
+                {props.coupon>0 ? <p>{" Your Generated Coupon is - "+props.coupon}</p>
+                :<p>you don't have Generated coupon  
+                </p> }  
+                
+                </div>  
+                <CheckoutList donotshowInputBox={true}/>  
+                <CheckoutSummary/>  
+                <button onClick={checkoutProcess}>
+                            Make Payment
+                </button>
+            </div>
+            :
+            <div className="checkoutComponent"> 
+                Thank you! Your Payment is in process
+            </div>
+        }
+        </React.Fragment>
+    )  
 }
