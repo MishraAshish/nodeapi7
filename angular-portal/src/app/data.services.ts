80b58f1e9fcf3273ed2a478f7a5bc3167eec8f7c
Ashish <ash.ishu025g@gmail.com>
2020-02-14 23:10:37 +0530

    added a number and any change event

--- angular-portal/src/app/data.services.ts
+++ angular-portal/src/app/data.services.ts
@@ -9,13 +9,19 @@ import { BehaviorSubject } from 'rxjs';
 
 export class DataService {
 
-    private messageSource = new BehaviorSubject<string>("default message");
-
+    private messageSource = new BehaviorSubject<any>("default message");
     currentMessage = this.messageSource.asObservable();
+
+    private messageSource1 = new BehaviorSubject<number>(10000);    
+    currentMessage1 = this.messageSource1.asObservable();
 
     constructor(){}
 
     changeMessage(newMessage: string){
         this.messageSource.next(newMessage) 
+    }
+
+    changeMessage1(newMessage: number){
+        this.messageSource1.next(newMessage) 
     }
 }
